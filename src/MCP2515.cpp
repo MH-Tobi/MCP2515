15c609698f3dafb46acfcc34b2cfb810e17fd25d
MH-Tobi <tobias_pflug@gmx.de>
2024-09-21 17:50:53 +0200

    Change Error-Handling getReceiveBufferDataLengthCode(): - remove check if initialized

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -3108,14 +3108,6 @@ uint8_t MCP2515::getReceiveBufferExtendedIdentifierLow(uint8_t BufferNumber)
  */
 uint8_t MCP2515::getReceiveBufferDataLengthCode(uint8_t BufferNumber)
 {
-  this->_lastMcpError = EMPTY_VALUE_16_BIT;
-
-  if (!_isInitialized)
-  {
-    this->_lastMcpError = ERROR_MCP2515_NOT_INITIALIZED;
-    return EMPTY_VALUE_8_BIT;
-  }
-
   if (BufferNumber > 1)
   {
     this->_lastMcpError = ERROR_MCP2515_VALUE_OUTA_RANGE;
