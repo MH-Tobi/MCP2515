88ffbb356e6967ae8cf3f03ba9c2c37b41c347f5
MH-Tobi <tobias_pflug@gmx.de>
2024-09-21 17:41:30 +0200

    Change Error-Handling getTransmitBufferDataLengthCode(): - remove check if initialized

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -2564,14 +2564,6 @@ bool MCP2515::setTransmitBufferExtendedIdentifierLow(uint8_t BufferNumber, uint8
  */
 uint8_t MCP2515::getTransmitBufferDataLengthCode(uint8_t BufferNumber)
 {
-  this->_lastMcpError = EMPTY_VALUE_16_BIT;
-
-  if (!_isInitialized)
-  {
-    this->_lastMcpError = ERROR_MCP2515_NOT_INITIALIZED;
-    return EMPTY_VALUE_8_BIT;
-  }
-
   if (BufferNumber > 2)
   {
     this->_lastMcpError = ERROR_MCP2515_VALUE_OUTA_RANGE;
