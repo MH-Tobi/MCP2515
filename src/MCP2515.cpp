d564927f0e5281ed0c3134cce76b19fe286a30fd
MH-Tobi <tobias_pflug@gmx.de>
2024-09-21 17:27:36 +0200

    Change Error-Handling getCanInterruptEnable(): - remove check if initialized - no Error will be set in this Method

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -1618,18 +1618,10 @@ bool MCP2515::modifyConfigurationRegister1(uint8_t Mask, uint8_t Value)
  *
  * 0 = Disabled
  *
- * On Error it will return EMPTY_VALUE_8_BIT (Check _lastMcpError).
+ * No Error will be set.
  */
 uint8_t MCP2515::getCanInterruptEnable()
 {
-  this->_lastMcpError = EMPTY_VALUE_16_BIT;
-
-  if (!_isInitialized)
-  {
-    this->_lastMcpError = ERROR_MCP2515_NOT_INITIALIZED;
-    return EMPTY_VALUE_8_BIT;
-  }
-
   return readInstruction(REG_CANINTE);
 }
 
