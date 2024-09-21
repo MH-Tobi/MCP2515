a2e883949b2fbccc94bceb3b54f681fc7c8b4f8c
MH-Tobi <tobias_pflug@gmx.de>
2024-09-21 17:16:37 +0200

    Change Error-Handling getTransmitErrorCounter(): - remove check if initialized - no Error will be set in this Method

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -1120,18 +1120,10 @@ bool MCP2515::modifyCanControl(uint8_t Mask, uint8_t Value)
  * @brief Get the Transmit Error Counter Register (REG_TEC) [1Ch]
  * @return uint8_t Transmit Error Count
  *
- * On Error it will return EMPTY_VALUE_8_BIT (Check _lastMcpError).
+ * No Error will be set.
  */
 uint8_t MCP2515::getTransmitErrorCounter()
 {
-  this->_lastMcpError = EMPTY_VALUE_16_BIT;
-
-  if (!_isInitialized)
-  {
-    this->_lastMcpError = ERROR_MCP2515_NOT_INITIALIZED;
-    return EMPTY_VALUE_8_BIT;
-  }
-
   return readInstruction(REG_TEC);
 }
 
