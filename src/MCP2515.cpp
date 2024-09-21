161099ea8633c79e4bdb7d2e02b10275c1cf040f
MH-Tobi <tobias_pflug@gmx.de>
2024-09-21 17:32:29 +0200

    Change Error-Handling getErrorFlag(): - remove check if initialized - no Error will be set in this Method

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -2055,18 +2055,10 @@ bool MCP2515::modifyCanInterruptFlag(uint8_t Mask, uint8_t Value)
  *
  * Resets when both REC and TEC are less than 96
  *
- * On Error it will return EMPTY_VALUE_8_BIT (Check _lastMcpError).
+ * No Error will be set.
  */
 uint8_t MCP2515::getErrorFlag()
 {
-  this->_lastMcpError = EMPTY_VALUE_16_BIT;
-
-  if (!_isInitialized)
-  {
-    this->_lastMcpError = ERROR_MCP2515_NOT_INITIALIZED;
-    return EMPTY_VALUE_8_BIT;
-  }
-
   return readInstruction(REG_EFLG);
 }
 
